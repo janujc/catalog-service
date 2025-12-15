1a8d0e15cd42fb89eabaae24cd430e9e147d4aa2
Januario Carreiro <33168291+janujc@users.noreply.github.com>
2025-12-14 19:27:41 -0500

    feat: create BookService

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -0,0 +1,21 @@
+package com.polarbookshop.catalogservice.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> viewBookList() {
+        return bookRepository.findAll();
+    }
+
+    public Book viewBookDetails(String isbn) {
+        return bookRepository.findByIsbn(isbn).orElseThrow(() -> new BookNotFoundException(isbn));
+    }
+
+}
