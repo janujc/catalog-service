a43d4d6fcbd71ab48fa858ab7c3a212e7ee2998d
Januario Carreiro <33168291+janujc@users.noreply.github.com>
2025-12-15 22:22:01 -0500

    refactor: move BookController to correct package

--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -1,4 +1,4 @@
-package com.polarbookshop.catalogservice;
+package com.polarbookshop.catalogservice.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
