02587dc9cd5e11bbd1ffda53bf2e7f72bbe9f199
Januario Carreiro <33168291+janujc@users.noreply.github.com>
2025-12-16 20:05:00 -0500

    test: add integration test

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package com.polarbookshop.catalogservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+import com.polarbookshop.catalogservice.domain.Book;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
 class CatalogServiceApplicationTests {
 
+	@Autowired
+	private RestTestClient restTestClient;
+
 	@Test
-	void contextLoads() {
+	void whenPostRequest_thenBookCreated() {
+		Book input = new Book("0123456789", "Title", "Author", 9.90);
+		Book expected = new Book("0123456789", "Title", "Author", 9.90);
+
+		restTestClient.post()
+				.uri("/books")
+				.body(input)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actual -> {
+					assertThat(actual).isEqualTo(expected);
+				});
 	}
 
 }
